83fea5ee484def963201b20f15c2ee6b74002f98
naklecha <nishant.aklecha@gmail.com>
2024-02-07 02:47:05 +0100

    cleaning up API responses and waiting for APIs to finish processing

--- app/api/uploaded/route.ts
+++ app/api/uploaded/route.ts
@@ -1,11 +1,13 @@
 import { getSession } from '@/app/supabase-server';
 import { onUpload } from '@/utils/supabase-admin';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   const response = await req.json();
   const session = await getSession();
-
   const output = response;
 
-  onUpload(output.document_id, session?.user.email as string);
+  await onUpload(output.document_id, session?.user.email as string);
+  
+  return NextResponse.json({document_id: output.document_id}, { status: 200 });
 }
