8a3da677341630415de1a59f53ec3bc6f016143b
naklecha <nishant.aklecha@gmail.com>
2024-02-08 00:27:07 +0100

    update static file

--- middleware.ts
+++ middleware.ts
@@ -11,17 +11,3 @@ export async function middleware(req: NextRequest) {
 
   return res
 }
-
-// Ensure the middleware is only called for relevant paths.
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!api\/check-processed).*)',
-  ],
-}
