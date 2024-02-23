2fffb9c55f76db0b01d5fd70f80ef805d306d603
Huzaifa Ahmad <huzaifa.a@berkeley.edu>
2024-02-22 22:15:52 -0800

    Copy updates

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,11 +1,9 @@
-import Link from 'next/link';
-import { createServerSupabaseClient } from '@/app/supabase-server';
-
-import Logo from '@/components/icons/Logo';
-import SignOutButton from './SignOutButton';
-
 import s from './Navbar.module.css';
 import SignInButton from './SignInButton';
+import SignOutButton from './SignOutButton';
+import { createServerSupabaseClient } from '@/app/supabase-server';
+import Logo from '@/components/icons/Logo';
+import Link from 'next/link';
 
 export default async function Navbar() {
   const supabase = createServerSupabaseClient();
@@ -21,8 +19,12 @@ export default async function Navbar() {
       <div className="max-w-6xl px-6 mx-auto text-sm">
         <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
           <div className="flex items-center flex-1">
-            <Link href="/" className="inline-flex items-center text-3xl font-bold" aria-label="Logo">
-              <img className='w-[100px]' src="/logo.png"></img>
+            <Link
+              href="/"
+              className="inline-flex items-center text-3xl font-bold"
+              aria-label="Logo"
+            >
+              <img className="w-[100px]" src="/logo.png"></img>
             </Link>
             {/* <nav className="hidden ml-6 space-x-2 lg:block">
               <Link href="/" className={s.link} >
@@ -36,15 +38,10 @@ export default async function Navbar() {
             </nav> */}
           </div>
           <div className="flex justify-end flex-1 space-x-8">
-
-            {user ? (
-              <SignOutButton />
-            ) : (
-              <SignInButton />
-            )}
+            {user ? <SignOutButton /> : <SignInButton />}
 
             {/* <button className='border border-[#E85533] px-4 py-2 text-[#E85533] font-bold rounded-full hidden md:block'>
-                Upload CSV
+                Upload Emails (.csv)
             </button> */}
           </div>
         </div>
