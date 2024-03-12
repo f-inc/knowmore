319b32cf920668f22cdc248cdbae726b74d1d1a7
Huzaifa Ahmad <huzaifa.a@berkeley.edu>
2024-03-12 11:21:55 -0700

    fix

--- app/dashboard/web3/page.tsx
+++ app/dashboard/web3/page.tsx
@@ -4,7 +4,6 @@ import {
   getSubscription
 } from '@/app/supabase-server';
 import Dashboard from '@/components/Dashboard';
-import Web3Dashboard from '@/components/Web3Dashboard';
 
 export default async function Web3Page() {
   const [session, products, subscription] = await Promise.all([
