c63baaf753f5c51ed62c05301097beb8964d393c
Amir <107724647+emrme@users.noreply.github.com>
2024-03-15 02:23:42 +0300

    add max duration

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,7 @@
 import { onPaid, processDomainDocument } from '@/utils/supabase-admin'
 
 export const maxDuration = 300
+export const dynamic = 'force-dynamic'
 
 export async function GET (req: Request) {
   const { searchParams } = new URL(req.url)
