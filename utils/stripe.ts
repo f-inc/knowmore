959cd92c0df368e3541b2639297e097b48fcdc89
Amir <107724647+emrme@users.noreply.github.com>
2024-03-15 18:20:04 +0300

    minor fix

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -1,10 +1,10 @@
-import { ensureEnvVar } from './helpers'
 import Stripe from 'stripe'
 
-ensureEnvVar('STRIPE_SECRET_KEY')
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+  // https://github.com/stripe/stripe-node#configuration
   apiVersion: '2022-11-15',
+  // Register this as an official Stripe plugin.
+  // https://stripe.com/docs/building-plugins#setappinfo
   appInfo: {
     name: 'Next.js Subscription Starter',
     version: '0.1.0'
